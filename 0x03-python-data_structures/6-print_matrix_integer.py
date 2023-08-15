36379f2b8c40602c94ad4e7481a9f305b45f5a99
Green <greenderrickngugi@gmail.com>
2023-08-15 14:54:07 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        count = len(row)
         for idx, integer in enumerate(row):
-            if idx != count - 1):
+            if idx != len(row) - 1):
                 print("{:d}".format(integer), end=" ")
             else:
                 print("{:d}".format(integer), end="")
