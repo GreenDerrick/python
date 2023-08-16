f89b47b9fc54a3b1b944a3fab9736f467b1ff88b
Green <greenderrickngugi@gmail.com>
2023-08-16 08:22:44 -0700

    used one import to call alll functions

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+a = 10
+b = 5
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    add(a, b)
+    sub(a, b)
+    mul(a, b)
+    div(a, b)
