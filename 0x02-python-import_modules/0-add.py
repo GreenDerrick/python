c1ce28533841e3852a28321598b49dd318136021
Green <greenderrickngugi@gmail.com>
2023-08-16 04:20:56 -0700

    changed the code to just print the return statement

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    import add_0
+    sum = add_0.add()
+    print("{}".format(sum))
