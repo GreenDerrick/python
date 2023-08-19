d4e5b938ba2d80e1908c3349bf22687fe5223417
Green <greenderrickngugi@gmail.com>
2023-08-19 11:29:26 -0700

    imported sys modue to use stdout.write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-__import__("sys").stdout.write("and that piece of art is")
-__import__("sys").stdout.write("useful - Dora Korpar, 2015-10-19\n")
+import sys
+
+sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
