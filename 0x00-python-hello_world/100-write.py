34f1d710982386ee41a54d1ff2b87973bdce0399
Green <greenderrickngugi@gmail.com>
2023-08-19 11:24:46 -0700

    implemented the __import__(sys)stdout.write() function

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-__import__("sys").stdout.write("and that piece of art is usefu - Dora Korpar, 2015-10-19\n")
+__import__("sys").stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
